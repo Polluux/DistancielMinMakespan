67188dec6db8e3cb3662aba760658e3cf1b37371
Roxane <roxane.bellot@gmail.com>
2018-01-02 19:05:05 +0100

    write in file

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -12,6 +12,7 @@ function au_clavier() {
 
 function alea() {
 	// get values
+	var filename = document.getElementById('input_alea_filename').value;
 	var n =   parseInt(document.getElementById('input_alea_n').value);
 	var m =   parseInt(document.getElementById('input_alea_m').value);
 	var k =   parseInt(document.getElementById('input_alea_k').value);
@@ -29,4 +30,60 @@ function alea() {
 		}
 		instances.push(new Instance(m, jobs));
 	}
+
+	// calculate instance
+	instances.map(x => x.calculate());
+
+	// write nicely in a file
+	
+	var txt = " ---~~ Roxane Bellot & Alexandre Boudine  ~~---\n \n \n \
+---~~ Entrées\n \n \
+nombre de machines : " + m + "\n \
+nombre de jobs: " + n + "\n \
+nombre d'instances : " + k + "\n \n \n \
+---~~ Résultats";
+
+
+	var rlsa = []
+	var rlpt = []
+	var rmyalgo = []
+
+	for (var i = 0; i < k; i++ ){
+		txt += "\n \n -- Instance n°" + i + "\n" + instances[i].toString();
+		var mi = Math.max(instances[i].borne_max, instances[i].borne_moy);
+		rlsa.push(instances[i].lsa / mi);
+		rlpt.push(instances[i].lpt / mi);
+		rmyalgo.push(instances[i].myalgo / mi);
+	}
+
+	// calculate stats
+
+	var sumrlsa = 0;
+	rlsa.map(x => parseInt (sumrlsa += x));
+	var sumrlpt = 0;
+	rlpt.map(x => parseInt (sumrlpt += x));
+	var sumrmyalgo = 0;
+	rmyalgo.map(x => parseInt (sumrmyalgo += x));
+
+	txt += "\n \n ---~~ Statistiques\n\n";
+	txt += "\n ratio d'approximation moyen LSA  : " + sumrlsa/rlsa.length;
+	txt += "\n ratio d'approximation moyen LPT  : " + sumrlpt/rlpt.length;
+	txt += "\n ratio d'approximation moyen MyAlgo  : " + sumrmyalgo/rmyalgo.length;
+
+
+
+
+	// download
+	// thanks https://stackoverflow.com/questions/3665115/
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(txt));
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+
 }
--- static/js/instance.js
+++ static/js/instance.js
@@ -9,6 +9,12 @@ class Instance {
 		this.lsa 	= lsa(this.nb_machines, this.jobs);
 		this.lpt 	= lpt(this.nb_machines, this.jobs);
 		this.myalgo = myalgo(this.nb_machines, this.jobs);
+		this.borne_max = Math.max(...this.jobs); // compatibilité ?;
+		var sum = 0;
+		for( var i = 0; i < this.jobs.length; i++ ){
+		    sum += this.jobs[i]
+		}
+		this.borne_moy = sum/this.nb_machines;
 		document.getElementById('loader').classList.remove('active');
 	}
 
@@ -16,11 +22,16 @@ class Instance {
 		document.getElementById('result_lsa').textContent = this.lsa; 
 		document.getElementById('result_lpt').textContent = this.lpt; 
 		document.getElementById('result_myalgo').textContent = this.myalgo; 
-		document.getElementById('borne_max').textContent = Math.max(...this.jobs); // compatibilité ?
-		var sum = 0;
-		for( var i = 0; i < this.jobs.length; i++ ){
-		    sum += this.jobs[i]
-		}
-		document.getElementById('borne_moy').textContent = sum/this.nb_machines;
+		document.getElementById('borne_max').textContent = this.borne_max
+		document.getElementById('borne_moy').textContent = this.borne_moy;
+	}
+
+	toString(){
+		var txt = "Borne inférieure \"maximum\" = " + this.borne_max + "\n";
+		txt += "Borne inférieure \"moyenne\" = " + this.borne_moy + "\n";
+		txt += "Résultat LSA = " + this.lsa + "\n";
+		txt += "Résultat LPT = " + this.lpt + "\n";
+		txt += "Résultat MyAlgo = " + this.myalgo + "\n";
+		return txt;
 	}
 }
