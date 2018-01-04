371bcf8f2d40da1f0af0d6115d5965520962771e
Roxane <roxane.bellot@gmail.com>
2018-01-04 11:50:23 +0100

    gestion d'erreur minimale

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## Par Roxane Bellot & Alexandre Boudine
 
 ## Accéder au projet
-Commencer par récupérer ce repository 
+Si ce n'est pas fait, récupérer ce repository 
 ```
 $ git clone https://github.com/Polluux/DistancielMinMakespan.git
 ```
@@ -11,3 +11,15 @@ Ouvrer ensuite *index.html* dans votre navigateur, par exemple avec
 ```
 $ firefox index.html
 ```
+ou en cliquant dessus dans un gestionnaire de fichiers.
+
+*Note* : Nous avons considéré que l'utilisateur final sait ce qu'il fait, la gestion d'erreurs est donc minimale.
+
+## Les algorithmes
+
+Les implémentations des algorithme LSA, LPT et MyAlgo en JavaScript sont dans *static/js/algos.js*.
+
+## Jeu de données
+
+Notre jeu de données se trouve dans *data/*.
+
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,8 +1,13 @@
 function au_clavier() {
-	single_instance(document.getElementById('input_au_clavier').value);
+	remove_errors();
+	if (!single_instance(document.getElementById('input_au_clavier').value)){
+		// error in prompt
+		display_error("Votre entrée contient des erreurs, veuillez les corriger", "input_form");
+	}
 }
 
 function alea() {
+	remove_errors();
 	// get values
 	var filename = document.getElementById('input_alea_filename').value;
 	var n =   parseInt(document.getElementById('input_alea_n').value);
@@ -28,12 +33,7 @@ function alea() {
 
 	// write nicely in a file
 	
-	var txt = " ---~~ Roxane Bellot & Alexandre Boudine  ~~---\n \n \n \
----~~ Entrées\n \n \
-nombre de machines : " + m + "\n \
-nombre de jobs: " + n + "\n \
-nombre d'instances : " + k + "\n \n \n \
----~~ Résultats";
+	var txt = " ---~~ Roxane Bellot & Alexandre Boudine  ~~---\n \n \n ---~~ Entrées\n \n nombre de machines : " + m + "\n nombre de jobs: " + n + "\n nombre d'instances : " + k + "\n \n \n ---~~ Résultats";
 
 
 	var rlsa = []
@@ -58,8 +58,8 @@ nombre d'instances : " + k + "\n \n \n \
 	rmyalgo.map(x => parseInt (sumrmyalgo += x));
 
 	txt += "\n \n ---~~ Statistiques\n\n";
-	txt += "\n ratio d'approximation moyen LSA  : " + sumrlsa/rlsa.length;
-	txt += "\n ratio d'approximation moyen LPT  : " + sumrlpt/rlpt.length;
+	txt += "\n ratio d'approximation moyen LSA     : " + sumrlsa/rlsa.length;
+	txt += "\n ratio d'approximation moyen LPT     : " + sumrlpt/rlpt.length;
 	txt += "\n ratio d'approximation moyen MyAlgo  : " + sumrmyalgo/rmyalgo.length;
 
 
@@ -82,12 +82,19 @@ nombre d'instances : " + k + "\n \n \n \
 
 
 function from_file() {
-	file = document.getElementById('input_from_file').files[0];
-	fr = new FileReader();
-	fr.onload = deal_with_it;
-	fr.readAsText(file)
+	remove_errors();
+	try {
+		file = document.getElementById('input_from_file').files[0];
+		fr = new FileReader();
+		fr.onload = deal_with_it;
+		fr.readAsText(file)
+	} catch(e){
+		display_error("Lire ce fichier est impossible", "file_form");
+	}
 	function deal_with_it() {
-		single_instance(fr.result);
+		if (!single_instance(fr.result)){
+			display_error("Ce fichier n'est pas de la bonne forme, veuillez le corriger ou en choisir un autre", "file_form");
+		}
 	}
 }
 
@@ -96,11 +103,28 @@ function single_instance(input) {
 	var nb_jobs = input[1];
 	var jobs = input.slice(2);
 
-	if (jobs.length == nb_jobs){
-		var instance = new Instance(input[0], jobs);
-		instance.calculate();
-		instance.populate();
+	if (jobs.length != nb_jobs){
+		return false;
 	}
 
+	var instance = new Instance(input[0], jobs);
+	instance.calculate();
+	instance.populate();
+
+	return true;
+}
+
+function display_error(text, id_parent) {
+	var node = document.createElement("p");
+	node.classList.add('error_message');
+	node.style = "color: #E61A8D";
+	node.appendChild(document.createTextNode(text));
+
+	document.getElementById(id_parent).appendChild(node);
+}
+
+function remove_errors() {
+	errors = Array.prototype.slice.call(document.getElementsByClassName('error_message'));
+	errors.forEach(function(elem) {elem.parentNode.removeChild(elem);});
 }
 
